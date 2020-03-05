2bc01d052695527bc154e97f12d920bab27ce9f1
George Yeranjian <georgeyeranjian@gmail.com>
2020-03-05 14:32:36 +0200

    modified mission tab with shadows

--- lib/Tabs/MissionTab.dart
+++ lib/Tabs/MissionTab.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:carousel_pro/carousel_pro.dart';
-import 'package:flutter/material.dart';
+
 
 
 
@@ -15,18 +14,62 @@ class _MissionTabState extends State<MissionTab> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Color.fromARGB(255, 35, 40, 50),
-        body: Column(
-            children:<Widget>[
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
               Row(
                 children: <Widget>[
-                  Container(
-                    child:Image.network("https://pbs.twimg.com/media/DX7DqY3WkAA-yt7.jpg"),
-                  ),
-                  Container(child: Text('The American University of Beirut (AUB) is an institution of higher learning founded to provide excellence in education, to participate  in the advancement of knowledge through research,and to serve the peoples of the Middle East and beyond. Chartered in New York State in 1863, the university bases its educational philosophy, standards, and practices on the American liberal arts model of higher education. ', style: TextStyle(color: Colors.white,fontStyle: FontStyle.italic),)),
+                  Container(height: 300,width: 410,
+                    margin: new EdgeInsets.all(8.0),
+                    decoration: new BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black,
+                          blurRadius: 20.0, // has the effect of softening the shadow
+                          spreadRadius: 1.0, // has the effect of extending the shadow
+                          offset: Offset(
+                            0, // horizontal, move right 10
+                            0, // vertical, move down 10
+                          ),
+                        )
+                      ]
+                    ),child: Image.network("https://blogbaladi.com/wp-content/uploads/2016/01/AUB-New-mindingthe-624x416.jpg"),),
+                  Expanded(child: Text('The American University of Beirut (AUB) is an institution of higher learning founded to provide excellence in education, to participate in the advancement of knowledge through research,and to serve the peoples of the Middle East and beyond. Chartered in New York State in 1863, the university bases its educational philosophy, standards, and practices on the American liberal arts model of higher education. ', style: TextStyle(color: Colors.white,fontStyle: FontStyle.italic,fontSize: 17),))
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('The university believes deeply in and encourages freedom of thought and expression and seeks to foster tolerance and respect for diversity and dialogue. Graduates will be individuals committed to creative and critical thinking, life-long learning, personal integrity, civic responsibility, and leadership. ', style: TextStyle(color: Colors.white,fontStyle: FontStyle.italic,fontSize: 17),),
+                  )),
+                  Container(height: 310.0,width: 410,
+                    margin: new EdgeInsets.all(8.0),
+                    decoration: new BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black,
+                          blurRadius: 20.0, // has the effect of softening the shadow
+                          spreadRadius: 1.0, // has the effect of extending the shadow
+                          offset: Offset(
+                            -5.0, // horizontal, move right 10
+                            0.0, // vertical, move down 10
+                          ),
+                        )
+                      ],
+                     
+                    ),
+                    child: Image.network("https://media-cdn.tripadvisor.com/media/photo-s/13/17/de/e1/aub-main-gate.jpg"),)
                 ],
               )
-            ]
+            ],
+          ),
         )
+
     );
   }
+
 }
+
+
+
