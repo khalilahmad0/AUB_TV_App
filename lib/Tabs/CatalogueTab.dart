c3140749b5bfc6a078ba2cd76a3e3c417cb7fdde
George_Y <gty00@mail.aub.edu>
2020-03-23 23:16:29 +0200

    New Commit for mounir

--- lib/Tabs/CatalogueTab.dart
+++ lib/Tabs/CatalogueTab.dart
@@ -1,4 +1,10 @@
 import 'package:aub/Pages/DetailPage.dart';
+import 'package:aub/Pages/FAFS.dart';
+import 'package:aub/Pages/FAS.dart';
+import 'package:aub/Pages/FHS.dart';
+import 'package:aub/Pages/FM.dart';
+import 'package:aub/Pages/MSFEA.dart';
+import 'package:aub/Pages/OSB.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:aub/Widgets/Cover2.dart';
@@ -12,22 +18,21 @@ class CatalogueTab extends StatefulWidget {
 
 
 class _CatalogueTabState extends State<CatalogueTab> {
-  Widget build(BuildContext context){
+    Widget build(BuildContext context){
     List<Cover2> myList = List<Cover2>();
-    myList.add(Cover2(text: "Faculty of Arts and Sciences (FAS)", image:"assets/FAS.JPG" ,  onTap: null));
-    myList.add(Cover2(text: "Faculty of Agricultural and Food Sciences (FAFS)", image:"assets/FAFS.jpg", onTap: null));
-    myList.add(Cover2(text: "Faculty of Health Sciences (FHS)", image:"assets/FHS.jpg", onTap: null));
-    myList.add(Cover2(text: "Faculty of Medicine (FM)", image:"assets/FM.jpg", onTap: null));
-    myList.add(Cover2(text: "Rafic Hariri School of Nursing", image:"assets/HSN.jpg", onTap: null));
-    myList.add(Cover2(text: "Maroun Semaan Faculty of Engineering and Architecture (MS​FEA)", image:"assets/MSFEA.jpg", onTap: null));
-    myList.add(Cover2(text: "Suliman S. Olayan School of Business (OSB)", image:"assets/OSB.jpg", onTap: null));
+    myList.add(Cover2(text: "Faculty of Arts and Sciences (FAS)", image:"assets/FAS.JPG" ,  onTap: () {Navigator.push(context, MaterialPageRoute(builder: (context) => FAS()));}));
+    myList.add(Cover2(text: "Faculty of Agricultural and Food Sciences (FAFS)", image:"assets/FAFS.jpg", onTap:() {Navigator.push(context, MaterialPageRoute(builder: (context) => FAFS()));}));
+    myList.add(Cover2(text: "Faculty of Health Sciences (FHS)", image:"assets/FHS.jpg",  onTap:() {Navigator.push(context, MaterialPageRoute(builder: (context) => FHS()));}));
+    myList.add(Cover2(text: "Faculty of Medicine (FM)", image:"assets/FM.jpg", onTap:() {Navigator.push(context, MaterialPageRoute(builder: (context) => FM()));}));
+    myList.add(Cover2(text: "Maroun Semaan Faculty of Engineering and Architecture (MSFEA)", image:"assets/MSFEA.jpg",  onTap:() {Navigator.push(context, MaterialPageRoute(builder: (context) => MSFEA()));}));
+    myList.add(Cover2(text: "Suliman S. Olayan School of Business (OSB)", image:"assets/OSB.jpg", onTap:() {Navigator.push(context, MaterialPageRoute(builder: (context) => OSB()));}));
 
     return new Scaffold(
-      backgroundColor: Color.fromARGB(255, 35, 40, 50),
-      body: new GridView.count(
-        crossAxisCount: 4,
-        children: myList,
-      ),
+    backgroundColor: Color.fromARGB(255, 35, 40, 50),
+    body: new GridView.count(
+    crossAxisCount: 4,
+    children: myList,
+    ),
     );
   }
 
